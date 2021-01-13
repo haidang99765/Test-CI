21fca108e15232d78461799ce5359e9a42790c8f
DESKTOP-ALPS9AN\Itachi <hoanghaidang2407@gmail.com>
2021-01-13 12:52:28 +0700

    update xoso

--- xoso.js
+++ xoso.js
@@ -1,9 +1,41 @@
-var number = document.getElementById('three');
-function quayso() {
-    if (number.value > 1 & number.value < 10) {
-        alert ('Hello world');
-    }
-    else {
-        alert ('Wrong');
+let Roll = document.getElementById('btn');
+let txtNumber = document.getElementById('three');
+let randomNumbe = getRandomNumber();
+let count = 0;
+function getRandomNumber() {
+    randomNumber = Number(Math.floor(Math.random() * 10) + 1);
+    return randomNumber;
+}
+function checkInputNumber() {
+    let number = txtNumber.value;
+    while (true) {
+        if (number === "") {
+            alert('Vui lòng nhập số');
+            break;
+        } else if (isNaN(number)) {
+            alert('Vui lòng nhập số!');
+            break;
+        } else if (number < 1 || number > 10) {
+            alert('Số nhập cần nằm trong khoảng 0-10!');
+            break;
+        } else {
+            count++;
+            return number;
+        }
     }
 }
+Roll.addEventListener('click', () => {
+    let number = checkInputNumber();
+    if (number == randomNumber) {
+        alert('Chúc mừng, bạn đã đoán đúng số may mắn!');
+        count = 0;
+    }
+    if (!isNaN(number) && (number <= 10 && number >= 1)) {
+        alert(`Sai! Còn lại ${3 - count} lần thử`);
+    }
+    if (count == 3) {
+        alert(`Bạn đã thua! Con số may mắn là:  ${randomNumber}`);
+        alert("Chơi lại nào!!!")
+        window.location.assign("xoso.html");
+    }
+});
