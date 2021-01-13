eaa9d51f4f7150412e7a4088f210a947a4d7b0c7
DESKTOP-ALPS9AN\Itachi <hoanghaidang2407@gmail.com>
2021-01-13 12:09:46 +0700

    test 2

---
+++ bai1.js
@@ -0,0 +1,8 @@
+function findOppositeNumber (n, InputNumber) {
+    if (n % 2 == 1) return;
+    else return (InputNumber + n / 2) % n;
+}
+
+console.log (findOppositeNumber(10, 2));
+console.log (findOppositeNumber(10, 6));
+console.log (findOppositeNumber(10, 5));
--- xoso.js
+++ xoso.js
@@ -1,6 +1,9 @@
 var number = document.getElementById('three');
 function quayso() {
-    if (number > 1 && number < 10) {
+    if (number.value > 1 & number.value < 10) {
         alert ('Hello world');
+    }
+    else {
+        alert ('Wrong');
     }
 }
