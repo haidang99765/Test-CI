a94ece1f652e018510d33927916f1c3faf0ca94c
DESKTOP-ALPS9AN\Itachi <hoanghaidang2407@gmail.com>
2021-01-13 12:11:35 +0700

    Update bai1.js

--- bai1.js
+++ bai1.js
@@ -6,3 +6,4 @@ function findOppositeNumber (n, InputNumber) {
 console.log (findOppositeNumber(10, 2));
 console.log (findOppositeNumber(10, 6));
 console.log (findOppositeNumber(10, 5));
+console.log (findOppositeNumber(10, 3));
